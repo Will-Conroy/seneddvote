d85d19b0bab95c4882f363fa1557a591f2b68b32
Will <william1conroy1@gmail.com>
2021-03-12 15:39:47 +0000

    Added max zoom out to 7 fir double maps

--- resources/views/Years/show.blade.php
+++ resources/views/Years/show.blade.php
@@ -1,71 +1,44 @@
 @extends('layouts.doubleMap')
-
 @section('title', 'Year')
 
 
+@section('seats')
+
+@endsection
+
 @section('map1')
-    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
-    <div id="regionalMap" style="width: 500px; height: 700px;"></div>
-    <script>
-        <!-- Making map, location and then zoom-->
-        var regionalMap = L.map('regionalMap').setView([52.5, -4.01], 8);
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-            maxZoom: 18,
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1
-        }).addTo(regionalMap);
-    </script>
 
     @foreach ($regions as $region)
         <script>        
             var regionName = {!! json_encode($region->name) !!};
             var regionCoords = {!! json_encode($coordinates[$region->name]) !!};
-            L.polygon(regionCoords).addTo(regionalMap).bindPopup(regionName);
+            L.polygon(regionCoords).addTo(map1).bindPopup(regionName);
             function onMapClick(e) 
             {
                 popup
                     .setLatLng(e.latlng)
                     .setContent( e.latlng.toString())
-                    .openOn(regionalMap);
+                    .openOn(map1);
             }
-            regionalMap.on('click', onMapClick);
+            map1.on('click', onMapClick);
         </script>
     @endforeach
 @endsection
 
 @section('map2')
-    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
-    <div id="constituencyMap" style="width: 500px; height: 700px;"></div>
-    <script>
-        <!-- Making map, location and then zoom-->
-        var constituencyMap = L.map('constituencyMap').setView([52.5, -4.01], 8);
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-            maxZoom: 18,
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1
-        }).addTo(constituencyMap);
-    </script>
     @foreach ($constituencies as $constituency)
         <script>        
             var constituencyName = {!! json_encode($constituency->name) !!};
             var constituencyCoords = {!! json_encode($coordinates[$constituency->name]) !!};
-            L.polygon(constituencyCoords).addTo(constituencyMap).bindPopup(constituencyName);
+            L.polygon(constituencyCoords, {color: 'red'}).addTo(map2).bindPopup(constituencyName);
             function onMapClick(e) 
             {
                 popup
                     .setLatLng(e.latlng)
                     .setContent( e.latlng.toString())
-                    .openOn(constituencyMap);
+                    .openOn(map2);
             }
-            constituencyMap.on('click', onMapClick);
+            map2.on('click', onMapClick);
         </script>
     @endforeach
 @endsection
--- resources/views/layouts/doubleMap.blade.php
+++ resources/views/layouts/doubleMap.blade.php
@@ -29,7 +29,42 @@
      </div>
      @endif
      <div>
+        @yield('seats')
+        <script src="https://unpkg.comleaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+        <div id="map1" style="width: 550px; height: 700px;"></div>
+        <script>
+            <!-- Making map, location and then zoom-->
+            var map1 = L.map('map1').setView([52.5, -4.01], 8);
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                maxZoom: 18,
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                id: 'mapbox/streets-v11',
+                tileSize: 512,
+                zoomOffset: -1
+            }).addTo(map1);
+            bounds = new L.LatLngBounds(new L.LatLng(53.657661, -5.861206), new L.LatLng(51.230968, -2.318115));
+            map1._layersMinZoom=7;
+        </script>
         @yield('map1')
+
+        <div id="map2" style="width: 550px; height: 700px;"></div>
+        <script>
+            <!-- Making map, location and then zoom-->
+            var map2 = L.map('map2').setView([52.5, -4.01], 8);
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                maxZoom: 18,
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                id: 'mapbox/streets-v11',
+                tileSize: 512,
+                zoomOffset: -1
+            }).addTo(map2);
+            map2._layersMinZoom=7;
+         </script>
+        
         @yield('map2')
 
      </div>
