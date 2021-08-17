ebedcaec23826dae79b9d53d283961aba10b0742
Will-Conroy <35853403+Will-Conroy@users.noreply.github.com>
2021-08-17 23:53:32 +0100

    Layout established for the portfolio Pages

---
+++ resources/views/Info/portfolio.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.nav')
+
+
+        
+
+
+
+
+
+    @section('app')
+    <div class=" jumbotron jumbotron-fluid text-center">
+            @yield('header')
+            @if($errors->any())
+                <div>
+                    <h3>Errors:</h3>   
+                        @foreach ($errors->all() as $error)
+                            <p>{{$error}}</p>
+                        @endforeach
+                </div>
+            @endif
+            @if(session('message'))
+                <div>
+                    <h3>message:</h3>   
+                    <p>{{session('message')}}</p>
+                </div>
+            @endif
+            @yield('personalInfo')
+        </div>
+
+        <div class="alert alert-success text-center">
+        <h3>Personal Projects</h3>
+    
+        </div>
+        @yield('personal')
+        <div class=" alert alert-success text-center">
+        <h3>Academic Projects</h3>
+
+        </div>
+        @yield('academic')
+
+
+    </div>
+</body>
+
+    @endsection
+
+
+    
+    
+</html>
+
--- resources/views/Info/will.blade.php
+++ resources/views/Info/will.blade.php
@@ -1,48 +1,129 @@
-@extends('layouts.app')
+@extends('Info.portfolio')
+
 @section('header')
     <h1>Will</h1>
     <p>William Conroy's Portfolio</p>
 @endsection
 
 
+@section('personalInfo')
 
-    @section('content')
-    <h3>Personal Projects</h3>
-    <h4>Senedd Vote</h4>
-    <div class="row justify-content-center">
-        
-            <div class="col">
+
+@endsection
+
+@section('personal')
+
+
+<div class=" jumbotron jumbotron-fluid text-center">
+    <h2>Senedd Vote</h2>
+    <hr class="my-4">
+    <div class=" container-fluid text-left">
+        <div class="row ">
+        <div class="col-sm-2 text-center">
+            <h4 class="lead"> Links</h4>
+            <hr class="my-4">
+            </div>
+            
+            <div class="col-sm-4 justify-content-center">
             <img src="{{url('/will/SeneddVoteDemo.gif')}}" class="rounded float" alt="Animated Gif of maps">
             </div>
-            <div class="col">
-            text
-            </div>
-    </div>
-    <h4>Ant Game</h4>
-    <div class="row justify-content-center">
-            <div class="col">
-                image
-            </div>
-            <div class="col">
-            text
-            </div>
-    </div>
+            
+            <div class="col-sm">
+                
+                    <h4 class="lead">Laravel - PHP, mySQL, JavaScript</h4>
+                    <hr class="my-4">
+                    <p>
+                        fadskfjasdkljfaslkdjfaslkdjflaskdjf;laskdjfal; skdjfl;asdkjfljaf;dljfalksdhgahdsglkhsadgkljfakdsjfksdhag akldhsg
+                        dfkhad;sflkh
+                        gdahhsd;array_change_key_caseasdghk
+                        adkghjdsg
+                        agdkhas
+                        gadshkghsa
+                        gadsgklashdg
+                        agdhaskdlg
+                        adgklagh
+                        adsgdkghag
+                    </p>
 
-    <h3>Educational Projects</h3>
-    <h4>C++</h4>
-    <div class="row justify-content-center">
-            <div class="col">
-                image
+                </div>
             </div>
-            <div class="col">
-            text
-            </div>
-    </div>
-
-    @endsection
+        </div>
+    </div>   
+</div>
 
 
-    
-    
-</html>
+<div class=" jumbotron jumbotron-fluid text-center">
+    <h2>Local Multiplayer Card Game</h2>
+    <hr class="my-4">
+    <div class=" container-fluid text-left">
+        <div class="row ">
+        <div class="col-sm-2 text-center">
+            <h4 class="lead"> Links</h4>
+            <hr class="my-4">
+            </div>
+            
+            <div class="col-sm-4 justify-content-center">
+            <img src="{{url('/will/SeneddVoteDemo.gif')}}" class="rounded float" alt="Animated Gif of maps">
+            </div>
+            
+            <div class="col-sm">
+                
+                    <h4 class="lead">Unity - C#</h4>
+                    <hr class="my-4">
+                    <p>
+                        fadskfjasdkljfaslkdjfaslkdjflaskdjf;laskdjfal; skdjfl;asdkjfljaf;dljfalksdhgahdsglkhsadgkljfakdsjfksdhag akldhsg
+                        dfkhad;sflkh
+                        gdahhsd;array_change_key_caseasdghk
+                        adkghjdsg
+                        agdkhas
+                        gadshkghsa
+                        gadsgklashdg
+                        agdhaskdlg
+                        adgklagh
+                        adsgdkghag
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </div>   
+</div>
+
+
+
+
+
+
+@endsection
+@section('academic')
+<div class=" jumbotron jumbotron-fluid text-center">
+    <h2>Stats Wales Data Analysis Tool</h2>
+    <hr class="my-4">
+    <div class=" container-fluid text-left">
+        <div class="row ">
+        <div class="col-sm-2 text-center">
+            <h4 class="lead"> Links</h4>
+            <hr class="my-4">
+            </div>
+            
+            <div class="col-sm-4 justify-content-center">
+            <img src="{{url('/will/SeneddVoteDemo.gif')}}" class="rounded float" alt="Animated Gif of maps">
+            </div>
+            
+            <div class="col-sm">
+                
+                    <h4 class="lead">Advanged Object Orated Progaming C++</h4>
+                    <hr class="my-4">
+                    <p >C++ tool to read and filter Walsh data stored in JSON and CSV formats</p>
+                    <p>
+                        asgadfgf
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </div>   
+</div>
+
+@endsection
 
