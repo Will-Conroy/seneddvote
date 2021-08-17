af94550b56c024dba056426a502609fa73d69769
Will-Conroy <35853403+Will-Conroy@users.noreply.github.com>
2021-08-17 22:52:25 +0100

    made Nav.blade.php. That app inherites

--- resources/views/Info/will.blade.php
+++ resources/views/Info/will.blade.php
@@ -1,7 +1,48 @@
 @extends('layouts.app')
-
-
-@section('title', 'Years')
-
-@section('content')
+@section('header')
+    <h1>Will</h1>
+    <p>William Conroy's Portfolio</p>
 @endsection
+
+
+
+    @section('content')
+    <h3>Personal Projects</h3>
+    <h4>Senedd Vote</h4>
+    <div class="row justify-content-center">
+        
+            <div class="col">
+            <img src="{{url('/will/SeneddVoteDemo.gif')}}" class="rounded float" alt="Animated Gif of maps">
+            </div>
+            <div class="col">
+            text
+            </div>
+    </div>
+    <h4>Ant Game</h4>
+    <div class="row justify-content-center">
+            <div class="col">
+                image
+            </div>
+            <div class="col">
+            text
+            </div>
+    </div>
+
+    <h3>Educational Projects</h3>
+    <h4>C++</h4>
+    <div class="row justify-content-center">
+            <div class="col">
+                image
+            </div>
+            <div class="col">
+            text
+            </div>
+    </div>
+
+    @endsection
+
+
+    
+    
+</html>
+
